01fe80708c5a854bc9d5f371e339eb9ea1e734d0
anosg93-jpg <anosg93@gmail.com>
2026-08-22 18:40:54 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // قم بتغيير رقم الإصدار هنا (مثلاً v1.1 أو v2.0) كلما قمت برفع تعديلات جديدة في كود index.html
-const CACHE_NAME = 'dr-broast-v40.0';
+const CACHE_NAME = 'dr-broast-v41.0';
 
 // الملفات الأساسية للتخزين المؤقت للعمل بدون إنترنت وبسرعة فائقة
 const STATIC_ASSETS = [
